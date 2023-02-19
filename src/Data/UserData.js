5f37901c629afd38641343fd4396970a7c994659
Vikas Belal <belal.vikas.5@gmail.com>
2023-02-19 19:01:54 +0530

    Dynamic user data file added

---
+++ src/Data/UserData.js
@@ -0,0 +1,9 @@
+export const userDetails = {
+    "name": "Vikas Belal",
+    "email":"belal.vikas.5@gmail.com",
+    "phone": 8171167303,
+    "designation": "Web Developer",
+    "state": "Uttarkhand",
+    "country": "India",
+    "yearOfBirth": 1999
+}
